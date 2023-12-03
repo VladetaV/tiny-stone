367d5a5d7e493aa6c9bac8338211fd9268d901ad
VladetaV <vladetavujacic@gmail.com>
2023-12-03 15:07:11 +0100

    Zavrsen drugi domaci

--- src/test/java/week/second/JavaSeleniumTestNG.java
+++ src/test/java/week/second/JavaSeleniumTestNG.java
@@ -28,53 +28,73 @@ public class JavaSeleniumTestNG {
 
     // pre pokretanja testova kreiramo objekat klase Webdriver
     @BeforeMethod(alwaysRun = true)
-    public void instanceDriver(){
+    public void instanceDriver() {
         driver = WebDriverFabric.startBrowser("chrome");
     }
 
     // nakon testova gasimo chrome
     @AfterMethod(alwaysRun = true)
-    public void closeBrowser(){
+    public void closeBrowser() {
         if (driver != null)
             driver.quit();
     }
 
 
     @Test(dataProvider = "podaci")
-    public void Test(String username, String password){
+    public void Test(String username, String password) {
 
-        //instanicramo objekat klase LoginPage
-        LoginPage login = new LoginPage(driver);
-        // Vrsimo logovanje
-        login.typeOnUsernameFieldXPath(username);
-        login.typeOnPasswordFieldCSS(password);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        login.clickOnLoginButton();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
-        // prosao je prvi deo testa
-        login.getCurrentUrl();
+        if (password.equals("secret_sauce")) {
+            //instanicramo objekat klase LoginPage
+            LoginPage login = new LoginPage(driver);
+            // Vrsimo logovanje
+            login.typeOnUsernameFieldXPath(username);
+            login.typeOnPasswordFieldCSS(password);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            login.clickOnLoginButton();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+            // prosao je prvi deo testa
+            login.getCurrentUrl();
 
-        // Instanciramo objekat klase HomePage
-        HomePage home = new HomePage(driver);
-        home.clickAddToCartButton();
-        home.clickBmButton();
-        home.clickResetStateButton();
-        home.clickLogoutButton();
-        // proveravamo da li je bilo uspesno logoutovanje
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/");
-        // prosao je ceo test
-        System.out.println("CURRENT URL IS -> " + driver.getCurrentUrl());
+            // Instanciramo objekat klase HomePage
+            HomePage home = new HomePage(driver);
+            home.clickAddToCartButton();
+            home.clickBmButton();
+            home.clickResetStateButton();
+            home.clickLogoutButton();
+            // proveravamo da li je bilo uspesno logoutovanje
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+            // prosao je ceo test
+            System.out.println("CURRENT URL IS -> " + driver.getCurrentUrl());
+        } else { // ako smo usli ovde onda je uneta neispravna lozinka
+            LoginPage loginpage = new LoginPage(driver);
+            loginpage.typeOnUsernameFieldXPath(username);
+            loginpage.typeOnPasswordFieldCSS(password);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            loginpage.clickOnLoginButton();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            System.out.println("CURRENT URL IS -> " + driver.getCurrentUrl());
+            boolean could_find = driver.findElement(By.className(("error-message-container"))).isDisplayed();
+            // bilo koji od ovih assertova je pojedinacno dovoljan, ali sam ih svejedno sve stavio radi primera
+            // i dalje smo na login stranici
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+            // nadjen je error message box
+            Assert.assertTrue(could_find);
+
+
+        }
+
     }
 
     // pravimo dataprovider
     // dva reda podataka su za ispravno logovanje a jedan red je za neispravno logovanje
-    // pa ce samim tim 2 testa proci a jedan pasti
     @DataProvider
-    public Object[][] podaci(){
+    public Object[][] podaci() {
         return new Object[][]{
-                {"standard_user","secret_sauce"},
+                {"standard_user", "secret_sauce"},
                 {"visual_user", "secret_sauce"},
                 {"wrong_username", "wrong_password"},
         };
     }
+
 }
